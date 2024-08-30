f01ac431aafb47e9a7b3630a5580e74ad2a7f47a
ArbitChaudhary <chaudharyarbit45s@gmail.com>
2024-08-30 09:26:33 -0400

    completed

--- components/home/artists-lineup/artists_lineup.tsx
+++ components/home/artists-lineup/artists_lineup.tsx
@@ -1,10 +1,23 @@
+import Image from "next/image";
 import ArtistsCard from "./artist_card";
 import styles from "./artists_lineup.module.scss";
+import bgImage from "../../../public/bg-flower.png";
+
 function ArtistsLineup() {
   return (
     <>
       <div className={styles.container}>
         <div className={styles.artists_lineup}>
+          <div className={styles.bg_image}>
+            <Image
+              src={bgImage}
+              alt="BG"
+              height={100}
+              width={100}
+              layout="responsive"
+              style={{ objectFit: "contain", position: "relative" }}
+            />
+          </div>
           <h1 className={styles.header}> 2024 Artists lineup </h1>
           <div className={styles.artist_flex}>
             <ArtistsCard />
--- components/home/hero/hero.tsx
+++ components/home/hero/hero.tsx
@@ -1,3 +1,5 @@
+import React, { useCallback, useEffect, useState } from "react";
+import Confetti from "react-confetti";
 import Image from "next/image";
 import styles from "./hero.module.scss";
 import logo from "../../../public/3E-Logo.png";
@@ -5,12 +7,72 @@ import circle from "../../../public/Circle-Loader.png";
 import kite1 from "../../../public/kite1.png";
 import kite2 from "../../../public/kite2.png";
 import kite3 from "../../../public/kite3.png";
+import { motion } from "framer-motion";
 
 const Hero = () => {
+  const [isCelebrating, setIsCelebrating] = useState(true);
+  const [windowDimensions, setWindowDimensions] = useState<{
+    width: number;
+    height: number;
+  }>({ width: 0, height: 0 });
+
+  const updateDimensions = useCallback(() => {
+    if (typeof window !== "undefined") {
+      setWindowDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+  }, []);
+
+  useEffect(() => {
+    updateDimensions();
+    window.addEventListener("resize", updateDimensions);
+    const timer = setTimeout(() => {
+      setIsCelebrating(false);
+    }, 5000); // Duration of 2 seconds
+    return () => {
+      clearTimeout(timer);
+      window.removeEventListener("resize", updateDimensions);
+    };
+  }, [updateDimensions]);
+
   return (
     <>
       <div className={styles.container}>
         <div className={styles.hero}>
+          <div
+            style={{
+              position: "absolute",
+              // overflow: "hidden",
+              height: "20vh",
+              top: 0,
+              left: 0,
+              zIndex: 10,
+            }}
+          >
+            <motion.div
+              initial={{ x: "-100vw", opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              exit={{ x: "100vw", opacity: 0 }}
+              transition={{ duration: 0.5 }}
+              style={{
+                position: "absolute",
+                top: "50%",
+                left: "10%",
+                transform: "translateY(-50%)",
+              }}
+            >
+              {/* <h1>Victory!</h1> */}
+            </motion.div>
+            {isCelebrating && (
+              <ConfettiEffect
+                width={windowDimensions.width}
+                height={windowDimensions.height}
+                run={isCelebrating}
+              />
+            )}
+          </div>
           <div className={styles.banner}>
             {/* <div className={styles.banner_logo}>
               <Image
@@ -72,3 +134,23 @@ const Hero = () => {
 };
 
 export default Hero;
+
+// src/ConfettiEffect.js
+
+interface IConfetti {
+  width: number;
+  height: number;
+  run: boolean;
+}
+
+const ConfettiEffect = ({ width, height, run }: IConfetti) => {
+  return (
+    <Confetti
+      width={width}
+      height={height}
+      recycle={false}
+      numberOfPieces={200}
+      run={run}
+    />
+  );
+};
