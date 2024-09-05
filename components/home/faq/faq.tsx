11870b7a0c5886e4d906cb999798c304041fa524
ArbitChaudhary <chaudharyarbit45s@gmail.com>
2024-09-05 01:46:43 -0400

    faq updated

--- components/home/faq/faq.tsx
+++ components/home/faq/faq.tsx
@@ -4,34 +4,35 @@ import { AiOutlinePlus } from "react-icons/ai";
 import { AiOutlineMinus } from "react-icons/ai";
 import Image from "next/image";
 import bgFlower from "../../../public/bg-flower.png";
+import { faqs } from "./faq_list";
 
-interface IFAQs {
-  question: string;
-  reply: string;
-}
+// interface IFAQs {
+//   question: string;
+//   reply: string;
+// }
 
-const faqs: IFAQs[] = [
-  {
-    question: "What makes you different from other Firms?",
-    reply:
-      "At Launch Business Law, we distinguish ourselves with personalized, expert legal services in IP & trademark, franchise, and corporate law. Our tailored solutions, innovative approach, and commitment to clear communication ensure that your business receives precise and effective legal support. With a proven track record and global perspective, we’re dedicated to helping your business thrive",
-  },
-  {
-    question: "What makes you different from other Firms?",
-    reply:
-      "At Launch Business Law, we distinguish ourselves with personalized, expert legal services in IP & trademark, franchise, and corporate law. Our tailored solutions, innovative approach, and commitment to clear communication ensure that your business receives precise and effective legal support. With a proven track record and global perspective, we’re dedicated to helping your business thrive",
-  },
-  {
-    question: "What makes you different from other Firms?",
-    reply:
-      "At Launch Business Law, we distinguish ourselves with personalized, expert legal services in IP & trademark, franchise, and corporate law. Our tailored solutions, innovative approach, and commitment to clear communication ensure that your business receives precise and effective legal support. With a proven track record and global perspective, we’re dedicated to helping your business thrive",
-  },
-  {
-    question: "What information should i provide to case?",
-    reply:
-      "At Launch Business Law, we distinguish ourselves with personalized, expert legal services in IP & trademark, franchise, and corporate law. Our tailored solutions, innovative approach, and commitment to clear communication ensure that your business receives precise and effective legal support. With a proven track record and global perspective, we’re dedicated to helping your business thrive",
-  },
-];
+// const faqs: IFAQs[] = [
+//   {
+//     question: "What makes you different from other Firms?",
+//     reply:
+//       "At Launch Business Law, we distinguish ourselves with personalized, expert legal services in IP & trademark, franchise, and corporate law. Our tailored solutions, innovative approach, and commitment to clear communication ensure that your business receives precise and effective legal support. With a proven track record and global perspective, we’re dedicated to helping your business thrive",
+//   },
+//   {
+//     question: "What makes you different from other Firms?",
+//     reply:
+//       "At Launch Business Law, we distinguish ourselves with personalized, expert legal services in IP & trademark, franchise, and corporate law. Our tailored solutions, innovative approach, and commitment to clear communication ensure that your business receives precise and effective legal support. With a proven track record and global perspective, we’re dedicated to helping your business thrive",
+//   },
+//   {
+//     question: "What makes you different from other Firms?",
+//     reply:
+//       "At Launch Business Law, we distinguish ourselves with personalized, expert legal services in IP & trademark, franchise, and corporate law. Our tailored solutions, innovative approach, and commitment to clear communication ensure that your business receives precise and effective legal support. With a proven track record and global perspective, we’re dedicated to helping your business thrive",
+//   },
+//   {
+//     question: "What information should i provide to case?",
+//     reply:
+//       "At Launch Business Law, we distinguish ourselves with personalized, expert legal services in IP & trademark, franchise, and corporate law. Our tailored solutions, innovative approach, and commitment to clear communication ensure that your business receives precise and effective legal support. With a proven track record and global perspective, we’re dedicated to helping your business thrive",
+//   },
+// ];
 
 function FAQ() {
   const [isOpen, setIsOpen] = useState<number | null>(null);
