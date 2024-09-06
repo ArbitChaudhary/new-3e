c2592378f6fc64a9f5443d1feb35f614224c218e
Iamnoob77 <68275507+Iamnoob77@users.noreply.github.com>
2024-09-06 12:43:27 +0545

    fixed : slider

--- components/home/why_attend/swiper/swiper.tsx
+++ components/home/why_attend/swiper/swiper.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
 import styles from "./swiper.module.scss";
-import { title } from "process";
 import Slider from "react-slick";
 
 interface SliderProps {
@@ -9,51 +8,64 @@ interface SliderProps {
 }
 
 function ContentSlider({ title, content }: SliderProps) {
-  const [activeIndex, setAactiveIndex] = useState<number>(0);
-  const [nav1, setNav1] = useState<Slider | null>(null);
-  const [nav2, setNav2] = useState<Slider | null>(null);
+  const [activeIndex, setActiveIndex] = useState<number>(0);
   const sliderRef1 = useRef<Slider>(null);
-  const sliderRef2 = useRef<Slider>(null);
+  const titleListRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    setNav1(sliderRef1.current);
-    setNav2(sliderRef2.current);
-  }, []);
+    if (sliderRef1.current) {
+      sliderRef1.current.slickGoTo(activeIndex);
+    }
+  }, [activeIndex]);
+
+  useEffect(() => {
+    if (titleListRef.current) {
+      const titleList = titleListRef.current;
+      const titleElement = titleList.children[activeIndex] as HTMLDivElement;
+
+      if (titleElement) {
+        const titleListWidth = titleList.offsetWidth;
+        const titleElementWidth = titleElement.offsetWidth;
+        const titleElementOffsetLeft = titleElement.offsetLeft;
+
+        const scrollLeft =
+          titleElementOffsetLeft + titleElementWidth / 2 - titleListWidth / 2;
+
+        titleList.scrollTo({
+          left: scrollLeft,
+          behavior: "smooth",
+        });
+      }
+    }
+  }, [activeIndex]);
 
   const handleClick = (index: number) => {
-    setAactiveIndex(index);
-    sliderRef2.current?.slickGoTo(index);
+    setActiveIndex(index);
   };
 
   const handleAfterChange = (index: number) => {
-    setAactiveIndex(index); // Update isActive when the slide changes
+    setActiveIndex(index);
   };
+
   return (
-    <div>
-      <div className={styles.list_container}>
+    <div className={styles.slider_container}>
+      {/* Title Navigation */}
+      <div className={styles.list_container} ref={titleListRef}>
         {title.map((item, index) => (
-          <Slider
-            asNavFor={nav1 ?? undefined}
-            ref={sliderRef2}
-            slidesToShow={1}
-            infinite={false}
-            variableWidth
-            arrows={false}
+          <div
+            key={index}
+            onClick={() => handleClick(index)}
+            className={
+              activeIndex === index ? styles.selected_title : styles.title
+            }
           >
-            <div
-              key={index}
-              onClick={() => handleClick(index)}
-              className={
-                activeIndex === index ? styles.selected_title : styles.title
-              }
-            >
-              {item}
-            </div>
-          </Slider>
+            {item}
+          </div>
         ))}
       </div>
+
+      {/* Content Slider */}
       <Slider
-        asNavFor={nav1 ?? undefined}
         ref={sliderRef1}
         swipeToSlide
         focusOnSelect
