afe9f578cdc19da856f7a72c0ce5632b5c644fc7
Iamnoob77 <68275507+Iamnoob77@users.noreply.github.com>
2024-09-07 19:00:24 +0545

    add : meta tags

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,55 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>DTFest 2024</title>
+        <meta
+          name="description"
+          content="DT Fest 2024 is a vibrant celebration of Nepali culture, featuring a mix of vendors, artisans, musicians, and dancers. Attendees can enjoy traditional dances, live performances, and authentic Nepalese products, art, and cuisine."
+        />
+        <meta
+          name="keywords"
+          content="dtfest, nepal, nepali culture, dashain america, festivals, tihar, dashain tihar fest, dashain tihar america, nepali events, dashain tihar dhamaka, dashain 2024, nepali dashain"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta name="organization" content="DTFest" />
+
+        <meta property="og:title" content="DTFest 2024" />
+        <meta
+          property="og:description"
+          content="DT Fest 2024 is a vibrant celebration of Nepali culture, featuring a mix of vendors, artisans, musicians, and dancers. Attendees can enjoy traditional dances, live performances, and authentic Nepalese products, art, and cuisine."
+        />
+        <meta property="og:image" content="../public/dtfest_logo.png" />
+        <meta
+          property="og:url"
+          content="https://main.d83nwks9nu6cc.amplifyapp.com/"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="DTFest 2024" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="DTFest 2024" />
+        <meta
+          name="twitter:description"
+          content="DT Fest 2024 is a vibrant celebration of Nepali culture, featuring a mix of vendors, artisans, musicians, and dancers. Attendees can enjoy traditional dances, live performances, and authentic Nepalese products, art, and cuisine."
+        />
+        <meta name="twitter:image" content="../public/dtfest_logo.png" />
+        <meta
+          name="twitter:url"
+          content="https://main.d83nwks9nu6cc.amplifyapp.com/"
+        />
+        <meta name="twitter:site" content="@dtfest" />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#ffffff" />
+        <meta
+          name="canonical"
+          content="https://main.d83nwks9nu6cc.amplifyapp.com/"
+        />
+
+        <link rel="icon" href="/favicon.ico" type="../public/dtfest_logo.png" />
+        <link rel="apple-touch-icon" href="../public/dtfest_logo.png" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,6 @@
-import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 import PrimaryLayout from "@/components/layout/primary_layout/primary_layout";
-import PageWrapper from "@/common/components/page_wrapper";
 import { NextPageWithLayout } from "./page";
-import { pages } from "next/dist/build/templates/app-page";
 import HomeLayout from "@/components/layout/pages/home/home_layout";
 import HomePage from "@/components/home";
 
@@ -13,12 +8,10 @@ const inter = Inter({ subsets: ["latin"] });
 
 const Home: NextPageWithLayout = () => {
   return (
-    <PageWrapper title="DTFest 2024">
-      <HomeLayout>
-        {/* <main className={`${styles.main} ${inter.className}`}>Home page</main> */}
-        <HomePage />
-      </HomeLayout>
-    </PageWrapper>
+    <HomeLayout>
+      {/* <main className={`${styles.main} ${inter.className}`}>Home page</main> */}
+      <HomePage />
+    </HomeLayout>
   );
 };
 
